296399616b334c6ddfea2ef8130ceaca6fddc9c0
RiddleTime <reinierklarenberg@gmail.com>
2020-04-20 20:20:32 +0200

    Update News
    Small refactor, Add description of FindImageSource method

--- PTLauncher/News.cs
+++ PTLauncher/News.cs
@@ -44,6 +44,10 @@ namespace PTLauncher.News
                 }
             }
 
+            /**
+             * <summary>Obtains the first image within the content of a new post</summary>
+             * It can filter 2 kind of images, them with some kind of steam preset {STEAM_CLAN_IMAGE} and the ones with that part replaced with the actual link.
+             */
             private string FindImageSource(string contents)
             {
                 const string clanImageBase = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/clans";
@@ -53,14 +57,12 @@ namespace PTLauncher.News
                 if (contents.Contains("STEAM_CLAN_IMAGE"))
                 {
                     image = clanImageBase + Regex.Match(contents, "((?<=STEAM_CLAN_IMAGE})(.[/s]*){1,60}((.png)|(.jpg)))").Value;
-                    return image;
                 }
                 else
                 {
                     image = Regex.Match(contents, "((" + clanImageBase + ")(.[/s]*){1,90}((.png)|(.jpg)))").Value;
                 }
 
-                Debug.WriteLine(image);
                 return image;
             }
         }
