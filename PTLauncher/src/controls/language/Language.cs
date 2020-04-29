bdba818c206892e92d651b3b0b9915a2f0f1263e
RiddleTime <reinierklarenberg@gmail.com>
2020-04-29 03:03:04 +0200

    Add a titlebar

--- PTLauncher/src/controls/language/Language.cs
+++ PTLauncher/src/controls/language/Language.cs
@@ -10,17 +10,17 @@ namespace PTLauncher.Language
         public string LocalizedName { get; set; }
 
         public static IList<GameLanguage> Languages = new List<GameLanguage>() {
-            new GameLanguage() { Name = "Dutch",        Parameter = "nl", LocalizedName = "Nederlands"},
+            new GameLanguage() { Name = "German",       Parameter = "de", LocalizedName = "Deutsch"},
             new GameLanguage() { Name = "English",      Parameter = "en", LocalizedName = "English"},
             new GameLanguage() { Name = "Estonian",     Parameter = "et", LocalizedName = "Eesti"},
+            new GameLanguage() { Name = "Spanish",      Parameter = "es", LocalizedName = "Español"},
             new GameLanguage() { Name = "French",       Parameter = "et", LocalizedName = "Français"},
-            new GameLanguage() { Name = "German",       Parameter = "de", LocalizedName = "Deutsch"},
-            new GameLanguage() { Name = "Hungarian",    Parameter = "hu", LocalizedName = "Magyar"},
             new GameLanguage() { Name = "Italian",      Parameter = "it", LocalizedName = "Italiano"},
+            new GameLanguage() { Name = "Hungarian",    Parameter = "hu", LocalizedName = "Magyar"},
+            new GameLanguage() { Name = "Dutch",        Parameter = "nl", LocalizedName = "Nederlands"},
             new GameLanguage() { Name = "Polish",       Parameter = "pl", LocalizedName = "Polskie"},
             new GameLanguage() { Name = "Portuguese",   Parameter = "pt", LocalizedName = "Português"},
             new GameLanguage() { Name = "Russian",      Parameter = "ru", LocalizedName = "Русский"},
-            new GameLanguage() { Name = "Spanish",      Parameter = "es", LocalizedName = "Español"},
             new GameLanguage() { Name = "Turkish",      Parameter = "tr", LocalizedName = "Türk"},
         };
     }
