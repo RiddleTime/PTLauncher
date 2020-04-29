55106233748deceac19668e05f9e32d564a14451
RiddleTime <reinierklarenberg@gmail.com>
2020-04-29 18:04:52 +0200

    Fix, Project Torque launcher can only be run once now

--- PTLauncher/App.xaml.cs
+++ PTLauncher/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using Microsoft.Shell;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace PTLauncher
@@ -11,7 +8,35 @@ namespace PTLauncher
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+
+
+        private const string Unique = "ProjectTorqueLauncher";
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+                application.InitializeComponent();
+                application.Run();
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // Bring window to foreground
+            if (this.MainWindow.WindowState == WindowState.Minimized)
+            {
+                this.MainWindow.WindowState = WindowState.Normal;
+            }
+
+
+            this.MainWindow.Activate();
+            return true;
+        }
     }
 }
